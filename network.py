188907f9f06c7c5f44877ab17c4af84d7e1cb21f
Nicolas.D <nico.devatine@gmail.com>
2020-04-30 14:42:39 +0200

    Initial commit

---
+++ network.py
@@ -0,0 +1,98 @@
+import math
+import torch.nn as nn
+import torch
+import numpy as np
+from fasttext import load_model
+from gensim.models.keyedvectors import KeyedVectors
+
+class FastText(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fasttext = KeyedVectors.load_word2vec_format('embeddings/wiki.en.align.vec', limit=5000)
+
+    def get_word_vector(self, word):
+        if not word in self.fasttext.vocab:
+            return np.zeros(300)
+
+        return self.fasttext[word]
+
+    def get_word_vectors(self, sentence):
+        return np.array([self.get_word_vector(word) for word in sentence])
+
+    def forward(self, sentence):
+        with torch.no_grad():
+            features = self.get_word_vectors(sentence)
+
+        return features
+
+class PermutationModule(nn.Module):
+    def __init__(self, args, input_dim, dropout=0.):
+        super(PermutationModule, self).__init__()
+
+        self.activation = torch.tanh
+        self.proj_dropout = nn.Dropout(dropout)
+
+        self.left_projection = torch.nn.Parameter(data=torch.Tensor(1, input_dim, args.proj_dim))
+        self.right_projection = torch.nn.Parameter(data=torch.Tensor(1, input_dim, args.proj_dim))
+        self.bias_projection = torch.nn.Parameter(data=torch.Tensor(1, 1, args.proj_dim))
+
+        self.output_projection = torch.nn.Parameter(data=torch.Tensor(args.proj_dim, 1))
+
+        self.initialize_parameters()
+
+    def initialize_parameters(self):
+        with torch.no_grad():
+            # using the default xavier init function is incorrect.
+            # indeed, we split the W matrix into 2 parts in order to reduce computation
+            # (trick from the papger of Kiperwasser and Goldberg)
+            # the pytorch function will compute the values with the fan_in / 2 instead of the real fan_in
+            # torch.nn.init.xavier_uniform_(self.head_projection)
+            # torch.nn.init.xavier_uniform_(self.mod_projection)
+            fan_in = self.left_projection.size()[1] * 2
+            fan_out = self.left_projection.size()[2]
+
+            std = 1.0 * math.sqrt(2.0 / float(fan_in + fan_out))
+            a = math.sqrt(3.0) * std  # Calculate uniform bounds from standard deviation
+            torch.nn.init.uniform_(self.left_projection, -a, a)
+            torch.nn.init.uniform_(self.right_projection, -a, a)
+
+            self.bias_projection.fill_(0.0)
+            
+            torch.nn.init.xavier_uniform_(self.output_projection)
+
+    def forward(self, input):
+        n_words = input.size()[0]
+
+        left_proj = input.matmul(self.left_projection)
+        right_proj = input.matmul(self.right_projection)
+
+        left_proj = left_proj.view(n_words, 1, -1)
+        right_proj = right_proj.view(1, n_words, -1)
+
+        values = left_proj + right_proj
+        
+        values = values.view(n_words * n_words, -1)
+        values = values + self.bias_projection
+        values = self.activation(values)
+        values = self.proj_dropout(values)
+
+        output = values[0].matmul(self.output_projection)
+
+        return output
+        
+class Network(nn.Module):
+    def __init__(self, args):
+        super(Network, self).__init__()
+        self.feature_extractor = FastText()
+        self.permutation = PermutationModule(args, 300)
+
+    def forward(self, input):
+        feature = self.feature_extractor(input)
+        feature = torch.from_numpy(feature).type(torch.FloatTensor)
+        ret = self.permutation(feature)
+        return ret
+
+    @staticmethod
+    def add_cmd_options(cmd):
+        cmd.add_argument('--proj-dim', type=int, default=128, help="Dimension of the output projection")
+        cmd.add_argument('--activation', type=str, default="tanh", help="activation to use in weightning modules: tanh, relu, leaky_relu")
---
+++ train.py
@@ -0,0 +1,137 @@
+import argparse
+import os
+import shutil
+import random
+import torch
+import numpy as np
+import pydestruct.data.conll as conll
+import pydestruct.algorithms.msa as msa
+import pydestruct.timer as timer
+import pydestruct.parser
+from gensim.models.keyedvectors import KeyedVectors
+
+import network
+
+def save_checkpoint(state, is_best, path, filename='checkpoint.pth.tar'):
+    torch.save(state, path + filename)
+    if is_best:
+        shutil.copyfile(path + filename, path + 'model_best.pth.tar')
+
+def eval(preds, golds):
+    total = 0
+    correct = 0
+    for p, g in zip(preds, golds):
+        for line_p, line_g in zip(p, g):
+            total += 1
+            for w_p, w_g in zip(line_p, line_g):
+                if w_g == 1 and w_p >= 0:
+                    correct += 1
+
+    return correct/total
+
+# Read command line
+cmd = argparse.ArgumentParser()
+cmd.add_argument("--train", type=str, required=True, help="Path to training data")
+cmd.add_argument("--dev", type=str, required=True, help="Path to dev data")
+cmd.add_argument("--model", type=str, required=True, help="Path where to store the model")
+cmd.add_argument("--format", type=str, default="conllu")
+cmd.add_argument("--lr", type=float, default=0.1)
+cmd.add_argument("--epochs", type=int, default=20, help="Number of epochs for training")
+cmd.add_argument("--batch", type=int, default=1, help="Mini-batch size")
+cmd.add_argument('--storage-device', type=str, default="cpu", help="Device where to store the data. It is useful to keep it on CPU when the dataset is large, even if computation is done on GPU")
+cmd.add_argument('--device', type=str, default="cpu", help="Device to use for computation")
+cmd.add_argument('--resume', type=str, default="", help="Resume training from a saved model.")
+network.Network.add_cmd_options(cmd)
+args = cmd.parse_args()
+
+print("Loading train and dev data")
+train_data = list(conll.read(args.train, format=args.format))
+dev_data = list(conll.read(args.dev, format=args.format))
+
+model = network.Network(args)
+
+model.to(device=args.device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+
+steps_per_epoch = len(list(range(0, len(train_data), 1)))
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 10 * steps_per_epoch, gamma=0.1)
+
+loss_builder = torch.nn.BCEWithLogitsLoss()
+
+for epoch in range(args.epochs):
+    print('Starting epoch', epoch)
+
+    random.shuffle(train_data)
+    model.train()
+    epoch_loss = 0
+    best_score = 0
+    best_epoch = 0
+    c = 0
+
+    for sentence in train_data:
+        optimizer.zero_grad()
+
+        n_words = len(sentence["tokens"])
+
+        if n_words < 2:
+            continue
+
+        if c % 500 == 0 and c != 0:
+            print("Sentence", c)
+
+        batch_inputs = [word["form"].lower() for word in sentence["tokens"]]
+
+        gold = torch.zeros((n_words, n_words))
+        for i in range(n_words - 1):
+            gold[i][i+1] = 1
+        gold = gold.reshape(-1, 1)
+
+        out = model(batch_inputs)
+        c += args.batch
+
+        mask = torch.ones((n_words, n_words)).fill_diagonal_(0).reshape(-1, 1)
+        loss_builder = torch.nn.BCEWithLogitsLoss(weight=mask)
+        loss = loss_builder(out, gold)
+
+        epoch_loss += loss.item()
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+
+    save_checkpoint({
+            'args': args,
+            'epoch': epoch + 1,
+            'state_dict': model.state_dict(),
+            'best_score': best_score,
+        }, False, args.model)
+
+    # dev evaluation
+    model.eval()
+
+    preds, golds = [], []
+
+    with torch.no_grad():
+        for sentence in dev_data:
+            n_words = len(sentence["tokens"])
+            gold = torch.zeros((n_words, n_words))
+            for i in range(n_words - 1):
+                gold[i][i+1] = 1
+            golds.append(gold)
+            
+            pred = model([word["form"].lower() for word in sentence["tokens"]])
+            pred = pred.view(n_words, n_words)
+            preds.append(pred)
+
+    score = eval(preds, golds)
+    print("Epoch %i | Loss %f | Score %f" % (epoch, epoch_loss, score))
+
+    if score > best_score:
+        best_score = score
+        best_epoch = epoch
+
+        save_checkpoint({
+            'args': args,
+            'epoch': epoch + 1,
+            'state_dict': model.state_dict(),
+            'best_score': best_score,
+        }, True, args.model)
