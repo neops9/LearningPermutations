dfd3705c697f2e391294aba9d741835c836cf384
Caio Filippo Corro <caio.corro@limsi.fr>
2020-05-06 12:18:54 +0200

    german

--- download_embs.sh
+++ download_embs.sh
@@ -7,3 +7,8 @@ rm wiki.en.align.vec
 wget https://dl.fbaipublicfiles.com/fasttext/vectors-aligned/wiki.fr.align.vec
 python filter_embs.py wiki.fr.align.vec embeddings/fr_ftb.vec data/fr_ftb/*.conllu
 rm wiki.fr.align.vec
+
+wget https://dl.fbaipublicfiles.com/fasttext/vectors-aligned/wiki.de.align.vec
+python filter_embs.py wiki.de.align.vec embeddings/de_hdt.vec data/de_hdt/*.conllu
+python filter_embs.py wiki.de.align.vec embeddings/de_gsd.vec data/de_gsd/*.conllu
+rm wiki.de.align.vec
