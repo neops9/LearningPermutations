695e1f00876bced13985f778d34a0ff20ac522b3
Nicolas.D <nico.devatine@gmail.com>
2020-09-04 16:37:51 +0200

    Update code

--- permute.py
+++ permute.py
@@ -10,10 +10,9 @@ import time
 import torch.nn as nn
 import learnperm.conll as conll
 import numpy as np
-import  re
+import re
 from learnperm.dict import build_tags_dictionnary
 import learnperm.special_tokens
-import reordering
 from scipy.stats import kendalltau
 
 def read_conllu(path, word_to_id, unk_idx, tags_dict, min_length=2, device="cpu"):
@@ -61,63 +60,6 @@ def load_embeddings(path, lang):
         n_embs += 1
     return data, word_to_id, id_to_word, n_embs
 
-def read_bias(path, n=2000, m=20):
-    embeddings_table, word_to_id, id_to_word, unk_idx = load_embeddings(args.bias_embeddings, args.lang)
-    checkpoint = torch.load(args.model, map_location=args.device)
-    checkpoint['args'].device = args.device
-    train_data = read_conllu(path, word_to_id, unk_idx, checkpoint["tags_dict"], device=args.storage_device)
-    train_size = len(train_data)
-
-    print("Loading model", flush=True)
-    model = network.Network(checkpoint['args'], embeddings_table=embeddings_table, add_unk=True, word_padding_idx=unk_idx, pos_padding_idx=checkpoint["tags_dict"].pad_index, n_tags=len(checkpoint["tags_dict"]._word_to_id.keys()))
-    model_state = model.state_dict()
-    model_state.update(checkpoint['state_dict'])
-    model.load_state_dict(model_state)
-    model.to(device=args.device)
-    model.eval()
-    
-    bigrams = list()
-
-    for i, sentence in enumerate(train_data):
-        n_words = len(sentence["tokens"])
-
-        batch_bigram, batch_start, batch_end = model([sentence])
-        bigram_weights = batch_bigram[0].detach()
-        start_weights = batch_start[0].detach()
-        end_weights = batch_end[0].detach()
-
-        if n_words > m:
-            continue
-            
-        bigrams.append((bigram_weights.contiguous(), start_weights.contiguous(), end_weights.contiguous()))
-        if len(bigrams) >= n:
-            break
-            
-    return bigrams
-
-def compute_tau(cpp_chart_array, weights, c, left_limit=-1, right_limit=-1):
-    taus = list()
-    for bi, s, e in weights:
-        n_words = bi.shape[0]
-        r = torch.arange(n_words)
-
-        bi[r[:-1], r[1:]] += c
-
-        p = reordering.argmax_n4(
-            cpp_chart_array,
-            bi.shape[0],
-            bi.data_ptr(),
-            s.data_ptr(),
-            e.data_ptr(),
-            left_limit,
-            right_limit
-        )
-        taus.append(kendalltau(list(range(len(p))), p).correlation)
-
-        bi[r[:-1], r[1:]] -= c
-
-    return sum(taus) / len(taus)
-
 # Read command line
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=str, required=True)
@@ -131,9 +73,7 @@ parser.add_argument("--format", type=str, default="conllu")
 parser.add_argument('--storage-device', type=str, default="cpu", help="Device where to store the data")
 parser.add_argument('--device', type=str, default="cpu", help="Device to use for computation")
 parser.add_argument('--verbose', action="store_true", help="")
-parser.add_argument('--bias', type=str, default="", help="Train corpus to compute bias")
-parser.add_argument('--bias-threshold', type=float, default=0.92, help="Threshold to select the bias")
-parser.add_argument('--bias-embeddings', type=str, help="Path to the bias embedding folder")
+parser.add_argument('--bias', type=float, default=0.0, help="Permutation bias")
 parser.add_argument('--output', type=str, default="output", help="Output file")
 parser.add_argument('--left-limit', type=str, default="-1")
 parser.add_argument('--right-limit', type=str, default="-1")
@@ -188,27 +128,10 @@ for i, sentence in enumerate(test_data):
     ends.append(batch_end)
     bigrams.append(batch_bigram)
 
-
 # add bias
-if args.bias != "":
-    bigrams_train = read_bias(args.bias, n=100, m=50)
-    cpp_chart = reordering.new_chart_n4(max(w[0].shape[0] for w in bigrams_train))
-    x = np.linspace(-10, 10, 50)
-    y = [compute_tau(cpp_chart, bigrams_train, i, 5, 5) for i in x]
-
-    bias = 0
-
-    for idx, v in enumerate(y):
-        if v >= args.bias_threshold:
-            bias = x[idx]
-            break
-
-    print("BIAS :", bias, file=sys.stderr, flush=True)
-
-    for i in range(len(bigrams)):
-        #print(bigrams[i].shape[0])
-        arange = np.arange(bigrams[i].shape[0])
-        bigrams[i][arange[:-1], arange[1:]] += bias
+for i in range(len(bigrams)):
+    arange = np.arange(bigrams[i].shape[0])
+    bigrams[i][arange[:-1], arange[1:]] += args.bias
 
 infile = open(input_filename, 'w')
 
@@ -234,7 +157,8 @@ cmd = [args.program, "--input", input_filename, "--algorithm", args.algo_version
 if args.verbose:
     cmd.append("--verbose")
 
-proc = subprocess.Popen(cmd).wait()
+proc = subprocess.check_output(cmd)
+print("Re-ordering output:", proc, file=sys.stderr, flush=True)
 
 # Regenerate conll file base on the program output
 def build_conll(path, orders):
@@ -265,5 +189,5 @@ outfile.close()
 output_path = args.output
 conll.write(output_path, build_conll(args.data, new_orders), format=args.format)
 
-#os.remove(input_filename)
-#os.remove(output_filename)
+os.remove(input_filename)
+os.remove(output_filename)
